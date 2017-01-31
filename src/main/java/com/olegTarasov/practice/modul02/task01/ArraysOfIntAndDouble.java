aed32e342e6a0e5357ee8e33f919c1e5c1dbc37d
Oleg Tarasov <tarasov.oleg@ukr.net>
2017-01-31 19:18:39 +0200

    Added checker for null in arrays.

--- src/main/java/com/olegTarasov/practice/modul02/task01/ArraysOfIntAndDouble.java
+++ src/main/java/com/olegTarasov/practice/modul02/task01/ArraysOfIntAndDouble.java
@@ -8,9 +8,11 @@ import java.util.Arrays;
 public class ArraysOfIntAndDouble {
     public static void main(String[] args) {
         ArraysOfIntAndDouble arraysOfIntAndDouble = new ArraysOfIntAndDouble();
-        int[] arrayInt = {5, 9, 10, 16, 10, 12, 21, 17, 12, 7};
-        double[] arrayDouble = {5, 9, 10, 16, 10, 12, 21, 17, 12, 7};
-        System.out.println();
+        //int[] arrayInt = {5, 9, 10, 16, 10, 12, 21, 17, 12, 7};
+        int[] arrayInt = {};
+        //double[] arrayDouble = {5, 9, 10, 16, 10, 12, 21, 17, 12, 7};
+        double[] arrayDouble = null;
+        System.out.println(" ");
         arraysOfIntAndDouble.sum(arrayInt);
         arraysOfIntAndDouble.max(arrayInt);
         arraysOfIntAndDouble.min(arrayInt);
@@ -18,6 +20,7 @@ public class ArraysOfIntAndDouble {
         arraysOfIntAndDouble.modul(arrayInt);
         arraysOfIntAndDouble.mult(arrayInt);
         arraysOfIntAndDouble.secLarge(arrayInt);
+
         System.out.println(" ");
         arraysOfIntAndDouble.sum(arrayDouble);
         arraysOfIntAndDouble.max(arrayDouble);
@@ -28,97 +31,182 @@ public class ArraysOfIntAndDouble {
         arraysOfIntAndDouble.secLarge(arrayDouble);
     }
 
+
     public void sum(int[] arr) {
-        int sumOfArrayElements = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sumOfArrayElements = sumOfArrayElements + arr[i];
+        if (arr == null || arr.length == 0) {
+            System.out.println("Your array is empty!");
+        } else {
+            int sumOfArrayElements = 0;
+            for (int i = 0; i < arr.length; i++) {
+                sumOfArrayElements = sumOfArrayElements + arr[i];
+            }
+            System.out.println("Sum of all int elements is " + sumOfArrayElements);
         }
-        System.out.println("Sum of all int elements is " + sumOfArrayElements);
     }
 
     public void min(int arr[]) {
-        Arrays.sort(arr);
-        System.out.println("Minimal value is " + arr[0]);
+        if (arr == null || arr.length == 0) {
+            System.out.println("Your array is empty!");
+        } else {
+            Arrays.sort(arr);
+            System.out.println("Minimal value is " + arr[0]);
+
+        }
     }
 
     public void max(int arr[]) {
-        Arrays.sort(arr);
-        System.out.println("Maximal value is " + arr[arr.length - 1]);
+        if (arr == null || arr.length == 0) {
+            System.out.println("Your array is empty!");
+        } else {
+            Arrays.sort(arr);
+            System.out.println("Maximal value is " + arr[arr.length - 1]);
+
+        }
+
     }
 
     public void maxPosit(int arr[]) {
-        Arrays.sort(arr);
-        int positive = arr[arr.length - 1];
-        if (positive >= 0) {
-            System.out.println("Maximal positive value is " + positive);
+        if (arr == null || arr.length == 0) {
+            System.out.println("Your array is empty!");
         } else {
-            System.out.println("All elements is below zero.");
+            Arrays.sort(arr);
+            int positive = arr[arr.length - 1];
+            if (positive >= 0) {
+                System.out.println("Maximal positive value is " + positive);
+            } else {
+                System.out.println("All elements is below zero.");
+            }
+
         }
+
     }
 
     public void mult(int arr[]) {
-        int multOfArrayElements = 1;
-        for (int i = 0; i < arr.length - 1; i++) {
-            multOfArrayElements = multOfArrayElements * arr[i];
+        if (arr == null || arr.length == 0) {
+            System.out.println("Your array is empty!");
+        } else {
+            int multOfArrayElements = 1;
+            for (int j = 0; j < arr.length - 1; j++) {
+                multOfArrayElements = multOfArrayElements * arr[j];
+            }
+            System.out.println("Multiplication of all values equals " + multOfArrayElements);
+
         }
-        System.out.println("Multiplication of all values equals " + multOfArrayElements);
+
     }
 
     public void modul(int arr[]) {
-        Arrays.sort(arr);
-        System.out.println("Modulus of first element is " + Math.abs(arr[0]) + "\nModulus of last element is " + Math.abs(arr[arr.length - 1]));
+        if (arr == null || arr.length == 0) {
+            System.out.println("Your array is empty!");
+        } else {
+            Arrays.sort(arr);
+            System.out.println("Modulus of first element is " + Math.abs(arr[0]) + "\nModulus of last element is " + Math.abs(arr[arr.length - 1]));
+
+        }
+
     }
 
     public void secLarge(int arr[]) {
-        Arrays.sort(arr);
-        int second = (arr[arr.length - 2]);
-        System.out.println("Second largest element is " + second);
+        if (arr == null || arr.length == 0) {
+            System.out.println("Your array is empty!");
+        } else {
+            Arrays.sort(arr);
+            int second = (arr[arr.length - 2]);
+            System.out.println("Second largest element is " + second);
+        }
+
     }
 
+
     public void sum(double[] arr) {
-        double sumOfArrayElements = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sumOfArrayElements = sumOfArrayElements + arr[i];
+        if (arr == null || arr.length == 0) {
+            System.out.println("Your array is empty!");
+        } else {
+            double sumOfArrayElements = 0;
+            for (int j = 0; j < arr.length; j++) {
+                sumOfArrayElements = sumOfArrayElements + arr[j];
+            }
+            System.out.println("Sum of all double elements is " + sumOfArrayElements);
+
         }
-        System.out.println("Sum of all double elements is " + sumOfArrayElements);
+
     }
 
     public void min(double[] arr) {
-        Arrays.sort(arr);
-        System.out.println("Minimal double value is " + arr[0]);
+        if (arr == null || arr.length == 0) {
+            System.out.println("Your array is empty!");
+        } else {
+            Arrays.sort(arr);
+            System.out.println("Minimal double value is " + arr[0]);
+
+        }
+
+
     }
 
     public void max(double[] arr) {
-        Arrays.sort(arr);
-        System.out.println("Maximal double value is " + arr[arr.length - 1]);
+        if (arr == null || arr.length == 0) {
+            System.out.println("Your array is empty!");
+        } else {
+            Arrays.sort(arr);
+            System.out.println("Maximal double value is " + arr[arr.length - 1]);
+
+        }
+
+
     }
 
     public void maxPosit(double[] arr) {
-        Arrays.sort(arr);
-        double positive = arr[arr.length - 1];
-        if (positive >= 0) {
-            System.out.println("Maximal positive value of double array is " + positive);
+        if (arr == null || arr.length == 0) {
+            System.out.println("Your array is empty!");
         } else {
-            System.out.println("All elements is below zero.");
+            Arrays.sort(arr);
+            double positive = arr[arr.length - 1];
+            if (positive >= 0) {
+                System.out.println("Maximal positive value of double array is " + positive);
+            } else {
+                System.out.println("All elements is below zero.");
+            }
+
         }
+
     }
 
     public void mult(double[] arr) {
-        double multOfArrayElements = 1;
-        for (int i = 0; i < arr.length - 1; i++) {
-            multOfArrayElements = multOfArrayElements * arr[i];
+        if (arr == null || arr.length == 0) {
+            System.out.println("Your array is empty!");
+        } else {
+            double multOfArrayElements = 1;
+            for (int j = 0; j < arr.length - 1; j++) {
+                multOfArrayElements = multOfArrayElements * arr[j];
+            }
+            System.out.println("Multiplication of all double values equals " + multOfArrayElements);
+
         }
-        System.out.println("Multiplication of all double values equals " + multOfArrayElements);
+
     }
 
     public void modul(double[] arr) {
-        Arrays.sort(arr);
-        System.out.println("Modulus of first element is " + Math.abs(arr[0]) + "\nModulus of last element is " + Math.abs(arr[arr.length - 1]));
+        if (arr == null || arr.length == 0) {
+            System.out.println("Your array is empty!");
+
+        } else {
+            Arrays.sort(arr);
+            System.out.println("Modulus of first element is " + Math.abs(arr[0]) + "\nModulus of last element is " + Math.abs(arr[arr.length - 1]));
+
+        }
+
     }
 
     public void secLarge(double[] arr) {
-        Arrays.sort(arr);
-        double second = (arr[arr.length - 2]);
-        System.out.println("Second largest element of double array is " + second);
+        if (arr == null || arr.length == 0) {
+            System.out.println("Your array is empty!");
+        } else {
+            Arrays.sort(arr);
+            double second = (arr[arr.length - 2]);
+            System.out.println("Second largest element of double array is " + second);
+
+        }
+
     }
 }
