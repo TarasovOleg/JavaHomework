029d7608a0bb4145cbb4229455a7f6b6133b0f48
Oleg Tarasov <tarasov.oleg@ukr.net>
2017-01-31 21:42:21 +0200

    Own "withdrawal" method changed on similar from WithdrawalFromArrays class

--- src/main/java/com/olegTarasov/practice/modul02/task04/FundTheBalance.java
+++ src/main/java/com/olegTarasov/practice/modul02/task04/FundTheBalance.java
@@ -1,12 +1,14 @@
 package com.olegTarasov.practice.modul02.task04;
 
 
+import com.olegTarasov.practice.modul02.task03.WithdrawalFromArrays;
+
 import java.util.Scanner;
 
 /**
  * Created by Олег on 29.01.2017.
  */
-public class FundTheBalance {
+public class FundTheBalance extends WithdrawalFromArrays {
     public static void main(String[] args) {
         FundTheBalance fundTheBalance = new FundTheBalance();
         double[] balances = {0, 1200, 250, 2000, 500, 3200};
@@ -18,13 +20,14 @@ public class FundTheBalance {
     }
 
 
-    public void selOwner(int a, double[] b, String[] c) {
-        if ((a == 0) || (a >= 6)) {
-            System.out.println("You entered wrong number");
+    public void selOwner(int numId, double[] balances, String[] ownerNames) {
+        WithdrawalFromArrays withdrawalFromArrays = new WithdrawalFromArrays();
+        if ((numId == 0) || (numId >= 6)) {
+            System.out.println("You entered wrong ID number");
         } else {
-            String ownerName = c[a];
-            double ownerBalance = b[a];
-            System.out.println("ID #" + a + ". " + ownerName + ", your balance is: " + ownerBalance + ".\n" +
+            String ownerName = ownerNames[numId];
+            double ownerBalance = balances[numId];
+            System.out.println("ID #" + numId + ". " + ownerName + ", your balance is: " + ownerBalance + ".\n" +
                     "Enter 1 for WITHDRAWAL, 2 for FUND or 3 for END.");
             Scanner yesOrNo = new Scanner(System.in);
             int check = yesOrNo.nextInt();
@@ -33,13 +36,13 @@ public class FundTheBalance {
                     System.out.println("Please, enter size of withdrawal");
                     Scanner cashWthd = new Scanner(System.in);
                     double wthdValue = cashWthd.nextDouble();
-                    withdrawal(a, b, c, wthdValue);
+                    withdrawalFromArrays.withdrawal(numId, balances, ownerNames, wthdValue);
                     break;
                 case 2:
                     System.out.println("Please, enter size of fund");
                     Scanner cashFnd = new Scanner(System.in);
                     double fndValue = cashFnd.nextDouble();
-                    fund(a, b, c, fndValue);
+                    fund(numId, balances, ownerNames, fndValue);
                     break;
                 case 3:
                     System.out.println("Good bye!");
@@ -50,19 +53,8 @@ public class FundTheBalance {
         }
     }
 
-    public void withdrawal(int a, double[] b, String[] c, double d) {
-        double comission = (d / 100) * 5;
-        double balance = b[a] - d - comission;
-        if (balance <= 0) {
-            System.out.println("Sorry, " + c[a] + ", there is not enough money.");
-        } else {
-            b[a] = balance;
-            System.out.println("OK, " + c[a] + " " + d + " " + b[a]);
-        }
-    }
-
-    public void fund(int a, double[] b, String[] c, double d) {
-        b[a] = b[a] + d;
-        System.out.println(c[a] + ". You funding " + d + ". Now your balance is: " + b[a]);
+    public void fund(int numId, double[] balances, String[] ownerNames, double sizeOfFund) {
+        balances[numId] = balances[numId] + sizeOfFund;
+        System.out.println(ownerNames[numId] + ". You funding " + sizeOfFund + "$. Now your balance is: " + balances[numId]);
     }
 }
