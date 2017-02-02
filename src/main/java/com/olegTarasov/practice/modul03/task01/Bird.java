2617fdd61ea98a293e5c6ce3470605845c597de2
Oleg Tarasov <tarasov.oleg@ukr.net>
2017-02-02 23:40:10 +0200

    Ready for tasks.

--- src/main/java/com/olegTarasov/practice/modul03/task01/Bird.java
+++ src/main/java/com/olegTarasov/practice/modul03/task01/Bird.java
@@ -4,19 +4,18 @@ package com.olegTarasov.practice.modul03.task01;
  * Created by Олег on 30.01.2017.
  */
 public class Bird {
+
+
     public static void main(String[] args) {
         Bird bird = new Bird();
-        String sing = "Kukareku";
-        bird.Sing(sing);
+        String song = "Kukareku";
+        bird.Sing(song);
 
     }
 
     public void Sing(String sing) {
-        String s = " I am walking\n" +
-                " I am flying\n" +
-                " I am singing\n" +
-                " I am Bird";
-        System.out.println(s);
+        String song = String.format(" I am walking%n I am flying%n I am singing%n I am Bird!");
+        System.out.println(song);
     }
 
 }
